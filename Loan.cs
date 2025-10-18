4a0b2f3345ad7b182e0f112338512b5a0db78414
jayce.baxter <jayce.baxter@proton.me>
2025-10-18 13:58:56 -0400

    init

---
+++ Loan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExercise2
+{
+    internal class Loan
+    {
+        private int loanNumber;
+        private string customerName;
+        private string customerAddress;
+        private double loanAmount;
+        private double interestRate;
+        private int loanDuration;
+
+        public void setLoanNumber(int loanNumber)
+        {
+            if (loanNumber > 0)
+            {
+                LoanNumber = loanNumber;
+            }
+            else
+            {
+                LoanNumber = 0;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassExercise2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReset_Click(object sender, RoutedEventArgs e)
+        {
+            TxtLoanNumberInput.Text = "";
+            TxtLoanNumberInput.IsReadOnly = false;
+
+            TxtCustomerNameInput.Text = "";
+            TxtCustomerNameInput.IsReadOnly = false;
+
+            TxtCustomerAddressInput.Text = "";
+            TxtCustomerAddressInput.IsReadOnly = false;
+
+            TxtLoanAmountInput.Text = "";
+            TxtLoanAmountInput.IsReadOnly = false;
+
+            TxtInterestRateInput.Text = "";
+            TxtInterestRateInput.IsReadOnly = false;
+
+            TxtLoanDuration.Text = "";
+            TxtLoanDuration.IsReadOnly = false;
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
